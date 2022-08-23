fa688a56122ac4edbc4c4ac55185dd7b045ff0cc
benaliozdemir <64617030+benaliozdemir@users.noreply.github.com>
2022-08-23 20:07:20 +0300

    Update config.py

--- BistBot/config.py
+++ BistBot/config.py
@@ -2,5 +2,5 @@ shareList = [
 "bist/bist30.txt",
 "bist/bist50.txt",
 "bist/bist100.txt"]
-telegramBotToken = "" #Telegram bot token ID
-telegramChatId = "" #Telegram bot chat ID
+telegramBotToken = "1138124161:AAEkfSqsNwH-9YOElk_J3Q8rAvg4BiNTn4E" 
+telegramChatId = "821818337" 
